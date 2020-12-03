54f11a3e1763da2e7edd95c8ce5a95e20a8e7e1c
wonmoLee <leewonmo94@gmail.com>
2020-12-03 20:25:34 +0900

    프로젝트 생성 및 DB연결, 서버연결 완료

---
+++ LILA-Server/lilaW/src/main/java/com/lila/gathering/LilaWApplication.java
@@ -0,0 +1,13 @@
+package com.lila.gathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilaWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilaWApplication.class, args);
+	}
+
+}
---
+++ LILA-Server/lilaW/src/test/java/com/lila/gathering/LilaWApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lila.gathering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilaWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
